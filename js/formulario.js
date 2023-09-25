9dd6ca7f7baecb74dacd2a12a27a73a690ba4944
CandelarioRamosj <145868728+CandelarioRamosj@users.noreply.github.com>
2023-09-25 17:13:54 -0600

    extraer input guarda la informacion de cada input en variables

--- js/formulario.js
+++ js/formulario.js
@@ -3,6 +3,18 @@ let boton = document.getElementById('enviar')
 boton.addEventListener('click',function(e){
  e.preventDefault()
   
-  let form = document.forms['formulario']
-  console.log(form)
+  let nombre = document.getElementById('name').value
+  let edad = document.getElementById('edad').value
+  let email = document.getElementById('email').value
+  let info = document.getElementById('info').value
+  let fecha = document.getElementById('fecha').value
+  let fav = document.querySelector('input[name="fav"]:checked')?.value
+  let zonas = ""
+  for(let i = 1; i <= 18; i++){
+    let isChecked = document.getElementById('zona'+ i).checked;
+    if(isChecked) zonas += document.getElementById('zona' + i).value + " "
+  }
+
+  let datos = document.getElementById('datos').value
+  alert(nombre + " " + edad + " " + email + " " + info + " " + fecha + " " + fav + " " + datos + " " + zonas)
 })
